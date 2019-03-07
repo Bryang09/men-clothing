32b4e44ae9f571e8193723a9b66ec1a10d3147f9
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-03-06 18:31:20 -0600

    Hero Section Completed

---
+++ client/src/Components/Landing/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <div className="text">
+        <h1>Bryan's Fashion for Men</h1>
+        <h3>Search Quality Clothing for Men</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
