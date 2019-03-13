073d8f9702f4ec32e5a09badf4b41417700f8789
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-03-13 15:35:04 -0500

    Items Responsive

--- client/src/Components/Items/Shared/Container/Items/Items.js
+++ client/src/Components/Items/Shared/Container/Items/Items.js
@@ -11,12 +11,7 @@ const Items = props => {
 
   const clothingData = data.map(res => {
     return (
-      <div
-        className={
-          res.type === "watches" ? "clothingItem watches" : "clothingItem"
-        }
-        key={res._id}
-      >
+      <div className={`clothingItem ${res.type}`} key={res._id}>
         <Link to={`${res._id}`}>
           <div className="img" style={{ backgroundImage: `url(${res.img})` }}>
             <div className="desc">
