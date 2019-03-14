7b47963e9daf2ff8d7f2e48f35addd5eb0740c5f
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-03-13 20:41:21 -0500

    Changed to Original.

--- server.js
+++ server.js
@@ -22,8 +22,6 @@ mongoose
 // USE ROUTES
 app.use("/api/clothing", clothing);
 
-const PORT = process.env.PORT || 5000;
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
@@ -31,5 +29,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
